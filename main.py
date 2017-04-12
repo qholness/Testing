b4f124a6ff9d704ef5cffa377f703e49c8c710b1
Quentin Holness <Quentin Holness>
2017-04-12 16:51:30 -0600

    commits

--- main.py
+++ main.py
@@ -15,3 +15,7 @@ def server_error(e):
     # Log the error and stacktrace.
     logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
+
+
+
+app.run(debug=True)
